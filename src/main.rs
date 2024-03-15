d69096cdee868a197685b6b256b2c2d45857bb1c
ddanielsantos <80872981+ddanielsantos@users.noreply.github.com>
2024-03-14 21:33:14 -0300

    style run cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
-use tokio::net::TcpListener;
 use crate::state::AppState;
+use tokio::net::TcpListener;
 
-mod state;
-mod domains;
 mod api;
 mod db;
+mod domains;
+mod state;
 
 #[tokio::main]
 async fn main() {
     let pool = db::create_connection_pool().await;
 
-    sqlx::migrate!().run(&pool).await.expect("Failed to migrate database");
+    sqlx::migrate!()
+        .run(&pool)
+        .await
+        .expect("Failed to migrate database");
     let state = AppState::new(pool);
 
-    let api = api::routes()
-        .with_state(state);
+    let api = api::routes().with_state(state);
 
-    let tcp_listener = TcpListener::bind("0.0.0.0:3000")
-        .await
-        .unwrap();
+    let tcp_listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
     axum::serve(tcp_listener, api).await.unwrap();
 }
